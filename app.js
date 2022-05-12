7aa7facecfadf85866de4eec6d264946d59f7776
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-12 18:09:36 -0500

    creando modelos y metodos de token y encriptacion de contraseña

--- app.js
+++ app.js
@@ -13,17 +13,22 @@ app.get('/', (_req, res) => {
     res.send('<h1>Jobs API</h1>')
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error)
-  }
-};
+
+    try {
+    
+        await connectDB(process.env.MONGO_URI)
+    
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        )
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
 
 start()
